e866ed6f92caac2124b0940975232f104b285569
lorenzoFerri95 <61703355+lorenzoFerri95@users.noreply.github.com>
2020-03-04 12:09:03 +0100

    File Handling
    funzione per cercare file nel file system

---
+++ file_handling.py
@@ -0,0 +1,24 @@
+import os
+import numpy as np
+import pandas as pd
+from collections import defaultdict
+
+
+"""
+https://stackoverflow.com/questions/1724693/find-a-file-in-python
+https://www.geeksforgeeks.org/os-walk-python/
+"""
+
+def findFile(fileName):
+    for root, dirs, files in os.walk(".", topdown=True):
+        if fileName in files:
+            return os.path.join(root, fileName)
+
+
+def main():
+    df = pd.read_csv(findFile("fileName"))
+    df.head()
+
+
+if __name__ == "__main__":
+    main()
