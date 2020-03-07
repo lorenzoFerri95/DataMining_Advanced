2d760e1956712252520b1c2ebb728364ea8b5400
lorenzoFerri95 <61703355+lorenzoFerri95@users.noreply.github.com>
2020-03-07 15:30:24 +0100

    preProcessing2

--- __Repo__/data_understanding.py
+++ __Repo__/data_understanding.py
@@ -8,21 +8,24 @@ import itertools
 from collections import defaultdict
 
 
-"""
-print('Primi Valori del Dataset:')
-df.head()
-print('Ultimi Valori del Dataset:')
-df.tail()
-print('Dimensioni del Dataset:\n',
-        'Numero di Oggetti: {}'.format(df.shape[0]), '\n',
-        'Numero di Attributi: {}'.format(df.shape[1]))
-print('Data Types degli Attributi:')
-df.dtypes
-print('Numero di Valori Nulli degli Attributi:')
-df.isnull().sum()
-print('Numero di Valori pari a 0 degli Attributi:')
-(df == 0).sum(axis=0)
-"""
+def main():
+    df = pd.read_csv('file')
+
+    print('Primi Valori del Dataset:')
+    df.head()
+    print('Ultimi Valori del Dataset:')
+    df.tail()
+    print('Dimensioni del Dataset:\n',
+            'Numero di Oggetti: {}'.format(df.shape[0]), '\n',
+            'Numero di Attributi: {}'.format(df.shape[1]))
+    print('Data Types degli Attributi:')
+    df.dtypes
+    print('Numero di Valori Nulli degli Attributi:')
+    df.isnull().sum()
+    print('Numero di Valori pari a 0 degli Attributi:')
+    (df == 0).sum(axis=0)
+
+
 
 """ ATTRIBUTI NUMERICI"""
 
@@ -58,19 +61,35 @@ def numeric_columns_boxplots(df, grouped_by=0):
         plt.show()
 
 
-def numeric_columns_scatterplot(df, grouped_by):
+def numeric_columns_scatterplot(df, grouped_by, transparence=1, gauss_kde=0):
 
     combs = itertools.combinations(get_numeric_columns(df), 2)
 
     for col_comb in combs:
-
+        
         plt.figure(figsize=(6, 4))
-        plt.scatter(df[col_comb[0]], df[col_comb[1]], c=df[grouped_by], s=15)
-
-        plt.title('Scatter Plot ' + col_comb[0] +
-                '  VS  ' + col_comb[1], pad=20, fontsize=15)
+        
+        if gauss_kde:
+            x = df[col_comb[0]].values
+            y = df[col_comb[1]].values
+            xy = np.vstack([x, y])
+            z = stats.gaussian_kde(xy)(xy)
+            
+            plt.scatter(x, y, c=z, s=10)
+        
+        else:
+            outClass = sorted(list(df[grouped_by].unique()))
+            
+            for i in outClass:
+                x = df[col_comb[0]].loc[df[grouped_by] == i].values
+                y = df[col_comb[1]].loc[df[grouped_by] == i].values
+                
+                plt.scatter(x, y,  label='Class = ' + str(i), s=10, alpha=transparence)
+ 
+        plt.title('Scatter Plot {}  VS  {}'.format(col_comb[0], col_comb[1]), pad=20, fontsize=15)
         plt.xlabel(col_comb[0], fontsize=10)
         plt.ylabel(col_comb[1], fontsize=10)
+        plt.legend()
         plt.show()
 
 
@@ -114,3 +133,9 @@ def get_categoric_columns(df):
     categoric_columns = [
         col for col in categoric_columns if df[col].nunique() < 50]
     return categoric_columns
+
+
+
+
+if __name__ == "__main__":
+    main()
